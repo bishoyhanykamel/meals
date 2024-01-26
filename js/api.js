47c1be78f48adc5c6179d5f1d5dfa04e5335ad20
Bishoy Hany <bishoyhany31@hotmail.com>
2024-01-26 16:31:08 +0200

    Add toggling for sidebar

--- js/api.js
+++ js/api.js
@@ -1,13 +1,11 @@
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 
-const getMeals = async function() {
-    const req = await fetch(`${BASE_URL}search.php?s=`);
-    return req.json();
-}
+const getMeals = async function () {
+  const req = await fetch(`${BASE_URL}search.php?s=`);
+  return await req.json();
+};
 
-
-const getMealByName = async function(searchIdx) {
-    const req = await fetch(`${BASE_URL}search.php?s=${searchIdx}`);
-    return req.json();
-}
-
+const getMealByName = async function (searchIdx) {
+  const req = await fetch(`${BASE_URL}search.php?s=${searchIdx}`);
+  return await req.json();
+};
--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
 "use strict";
 
 $(document).ready(() => {
-
-  const navbarContentWidth = $('#navbarContent').innerWidth();
-  $('#sideNavbar').css({left: `-=${navbarContentWidth}`});
-  $('#navbarToggler').click(() => {
-    let width = $('#sideNavbar').css('left');
-    console.log(width, 'hi');
-    if($('#sideNavbar').css('left') < 0)
-      $('#sideNavbar').animate({left: '0px'}, 1000);
-  })
-
-  $("#loader").fadeOut(1000, () => {
-
-    $("#loader").removeClass("d-flex");
-    $("body").removeClass("overflow-hidden");
-  });
+  setNavBar();
   // Get all meals to populate website
-  // const res = getMeals();
-  // res.then((data) => {
-  //   console.log(data);
-  //   $("#loader").fadeOut(1000, () => {
-  //     $("#loader").removeClass("d-flex");
-  //     $("body").removeClass("overflow-hidden");
-  //   });
-  // });
+  const res = getMeals();
+  res.then((data) => {
+    console.log(data);
+    $("#loader").fadeOut(1000, () => {
+      $("#loader").removeClass("d-flex");
+      $("body").removeClass("overflow-hidden");
+    });
+  });
 });
+
+function setNavBar() {
+  const navbarContentWidth = $("#navbarContent").innerWidth();
+  $("#sideNavbar").css({ left: `-=${navbarContentWidth}` });
+  $("#navbarToggler").click(() => {
+    let width = $("#sideNavbar").css("left");
+    if (width[0] !== "0") {
+      $("#sideNavbar").animate({ left: "0px" }, 500);
+    } else {
+      $("#sideNavbar").animate({ left: `-=${navbarContentWidth}` }, 500);
+    }
+  });
+}
