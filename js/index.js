28c59d6693bdc6bc89c3f47263ac94c137385a17
Bishoy Hany <bishoyhany31@hotmail.com>
2024-01-26 14:41:30 +0200

    Re-structure files

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-'use strict';
+"use strict";
 $(document).ready(() => {
-    const res = getMeals();
-    res.then((data) => {
-      console.log(data);
-      $("#loader").fadeOut(1000, () => {
-        $('#loader').removeClass('d-flex');
-      });
+
+  // Get all meals to populate website
+  const res = getMeals();
+  res.then((data) => {
+    console.log(data);
+    $("#loader").fadeOut(1000, () => {
+      $("#loader").removeClass("d-flex");
+      $("body").removeClass("overflow-hidden");
     });
   });
+});
