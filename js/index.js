9380a2e2bea63e2761b74ef3aecabf0db7760eb2
Bishoy Hany <bishoyhany31@hotmail.com>
2024-01-26 15:35:49 +0200

    Temporary update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 "use strict";
+
 $(document).ready(() => {
 
-  // Get all meals to populate website
-  const res = getMeals();
-  res.then((data) => {
-    console.log(data);
-    $("#loader").fadeOut(1000, () => {
-      $("#loader").removeClass("d-flex");
-      $("body").removeClass("overflow-hidden");
-    });
+  const navbarContentWidth = $('#navbarContent').innerWidth();
+  $('#sideNavbar').css({left: `-=${navbarContentWidth}`});
+  $('#navbarToggler').click(() => {
+    let width = $('#sideNavbar').css('left');
+    console.log(width, 'hi');
+    if($('#sideNavbar').css('left') < 0)
+      $('#sideNavbar').animate({left: '0px'}, 1000);
+  })
+
+  $("#loader").fadeOut(1000, () => {
+
+    $("#loader").removeClass("d-flex");
+    $("body").removeClass("overflow-hidden");
   });
+  // Get all meals to populate website
+  // const res = getMeals();
+  // res.then((data) => {
+  //   console.log(data);
+  //   $("#loader").fadeOut(1000, () => {
+  //     $("#loader").removeClass("d-flex");
+  //     $("body").removeClass("overflow-hidden");
+  //   });
+  // });
 });
